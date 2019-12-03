1b227ff1c992b905ba701980b333121e4f86e264
Darek Nyckowiak <dareknyckowiak@Dareks-MacBook-Pro.local>
2019-12-03 16:43:52 +0100

    contact

--- src/index.js
+++ src/index.js
@@ -12,18 +12,30 @@ import './index.css'
 import Importer from 'myComponents/Importer'
 import NotFound from 'myPages/NotFound'
 
+// Pages
 const About = Importer(() => import('myPages/About'))
 const Admin = Importer(() => import('myPages/Admin'))
 const Article = Importer(() => import('myPages/Article'))
 const Category = Importer(() => import('myPages/Category'))
 const Home = Importer(() => import('myPages/Home'))
 const Contact = Importer(() => import('myPages/Contact'))
+const What = Importer(() => import('myPages/What'))
+const How = Importer(() => import('myPages/How'))
+const Why = Importer(() => import('myPages/Why'))
+const Who = Importer(() => import('myPages/Who'))
+const Timeline = Importer(() => import('myPages/Timeline'))
 
+// Routing
 const App = () => (
   <BrowserRouter>
     <Titled title={() => 'Bioplan'}>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/what' component={What} />
+        <Route path='/how' component={How} />
+        <Route path='/why' component={Why} />
+        <Route path='/who' component={Who} />
+        <Route path='/timeline' component={Timeline} />
         <Route path='/contact' component={Contact} />
         <Route path='/about' component={About} />
         <Route path='/admin' component={Admin} />
